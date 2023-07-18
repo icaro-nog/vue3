fd41f3c8dffbbc153156352b8972001b0e31a079
Ícaro Nogueira <icaro.nogueira@brudam.com.br>
2023-07-18 19:41:50 -0300

    :books: docs: Atributos dinâmicos

--- 2_projeto_vue/src/components/Info.vue
+++ 2_projeto_vue/src/components/Info.vue
@@ -9,16 +9,24 @@
             <li>Python</li>
         </ul>
         <p v-show="mostrar_email">Mande uma mensagem para: {{ email }}</p>
+        <p>Acesse meu portfolio neste link: <a v-bind:href="meu_link" target="_blank">link</a> </p>
+        <Picture />
     </div>
 </template>
 <script>
+    import Picture from './Picture.vue';
+
     export default {
         name: 'Info',
+        components: {
+            Picture
+        },
         data() {
             return { 
                 esta_trabalhando: false,
                 mostrar_email: true,
-                email: 'icaro@teste.com.br'
+                email: 'icaro@teste.com.br',
+                meu_link: 'https://www.google.com.br'
             }
         }
     }
