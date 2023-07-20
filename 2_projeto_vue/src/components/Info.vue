ecc34800a81c8be56184b2d7f8882ef2a3f298e1
Ícaro Nogueira <icaro.nogueira@brudam.com.br>
2023-07-20 15:46:51 -0300

    :books: docs: Renderização de listas v-for part 2

--- 2_projeto_vue/src/components/Info.vue
+++ 2_projeto_vue/src/components/Info.vue
@@ -3,8 +3,12 @@
         <p v-if='esta_trabalhando'>Estou trabalhando no momento.</p>
         <p v-else>Estou em busca de novas oportunidades!</p>
         <p>Utilizo as seguintes tecnologias para backend:</p>
-        <ul v-for="technology in backend_technologies">
-            <li>{{ technology }}</li>
+        <ul>
+            <li v-for="(technology, index) in backend_technologies" v-bind:key="index" >{{ technology }}</li>
+        </ul>
+        <p>Utilizo as seguintes tecnologias para front-end</p>
+        <ul>
+            <li v-for="technology in frontend_technologies" :key="technology.id">{{ technology.language }}</li>
         </ul>
         <div>
             <button @click="showEmail">{{ texto_botao }}</button>
@@ -29,7 +33,12 @@
                 email: 'icaro@teste.com.br',
                 meu_link: 'https://www.google.com.br',
                 texto_botao: 'Mostrar e-mail',
-                backend_technologies: ['JavaScript', 'PHP', 'Python']
+                backend_technologies: ['JavaScript', 'PHP', 'Python'],
+                frontend_technologies: [
+                    {id: 1, language: 'HTML'},
+                    {id: 2, language: 'CSS'},
+                    {id: 3, language: ' Vue'},
+                ]
             }
         },
         methods: {
