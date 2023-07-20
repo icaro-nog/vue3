17346d2a960ce83bb5a95b2072958deed0a1abb0
Ícaro Nogueira <icaro.nogueira@brudam.com.br>
2023-07-20 17:34:47 -0300

    :books: docs: Reutilização de componentes

--- 2_projeto_vue/src/components/Picture.vue
+++ 2_projeto_vue/src/components/Picture.vue
@@ -1,10 +1,18 @@
 <template>
     <div>
         <img :src="avatar" :alt="descricao">
+        <p>aqui</p>
+        <Reutilizacao />
+        <p>aqui</p>
+        <p>aqui</p>
+        <p>aqui</p>
+        <p>aqui</p>
     </div>
 </template>
 
 <script>
+    import Reutilizacao from './Reutilizacao.vue'
+
     export default {
         name: 'Picture',
         data() {
@@ -12,6 +20,9 @@
                 avatar: '/img/avatar.png',
                 descricao: 'Ícaro'
             }
+        },
+        components: {
+            Reutilizacao
         }
     }
 </script>
--- 2_projeto_vue/src/components/PrimeiroComponente.vue
+++ 2_projeto_vue/src/components/PrimeiroComponente.vue
@@ -2,16 +2,22 @@
   <div>
     <h1>Olá Vue!</h1>
     <MultiplosEventos />
+    <Reutilizacao />
+    <Reutilizacao />
+    <Reutilizacao />
+    <Reutilizacao />
   </div>
 </template>
 
 <script>
   import MultiplosEventos from './MultiplosEventos.vue';
+  import Reutilizacao from './Reutilizacao.vue';
 
   export default {
     name: 'PrimeiroComponente',
     components: {
-      MultiplosEventos
+      MultiplosEventos,
+      Reutilizacao
     }
   }
 </script>
